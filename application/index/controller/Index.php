506ff96988f6b51219941d8d7067c76fd2675a01
equinox <equinoxsun@purplethunder.cn>
2017-08-07 02:38:37 +0800

    debug

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -499,6 +499,15 @@ class Index extends APIController
         }
     }
 
+    public function testPay()
+    {
+        $out_trade_no = '72170807023451722674';
+        $transaction_id = '2017080721001004530285082170';
+        $total_fee = 0.01;
+        $this->updateServiceExpense($out_trade_no,$transaction_id,$total_fee,1);
+        echo "1";
+    }
+
     /**
      * [updateServiceExpense description]
      * @param  string $value [description]
