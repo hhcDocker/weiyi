afa7c17dc501b7cea931b5dcecb8461098e461b5
Dengfuqiang <244146962@qq.com>
2017-10-12 09:32:23 +0800

    test环境测试

--- application/config.php
+++ application/config.php
@@ -249,8 +249,8 @@ return [
         'WXPAY_APPSECRET'   => '7813490da6f1265e4901ffb80afaa36f',//微信公众号appsecret
     ],
 
-    'PhantomjsPath' =>'/usr/bin/phantomjs', //执行文件位置
-     //'PhantomjsPath' => $_SERVER['DOCUMENT_ROOT'].'/../vendor/bin/phantomjs.exe',
+    //'PhantomjsPath' =>'/usr/bin/phantomjs', //执行文件位置
+     'PhantomjsPath' => $_SERVER['DOCUMENT_ROOT'].'/../vendor/bin/phantomjs.exe',
     'description_cache_day' =>1,//商品详情图文详情保留时间
     'tm_detail_cache_day' =>3,//天猫商品详情保留时间
 ];
